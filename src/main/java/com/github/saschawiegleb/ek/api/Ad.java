f4cf57c10b4d86644d930eb6777df55511ce7049
André Rouél <arouel@datameer.com>
2017-04-12 09:48:47 +0200

    Ad is a value type

--- src/main/java/com/github/saschawiegleb/ek/api/Ad.java
+++ src/main/java/com/github/saschawiegleb/ek/api/Ad.java
@@ -1,48 +1,41 @@
 package com.github.saschawiegleb.ek.api;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatterBuilder;
 
+import org.immutables.value.Value.Default;
+import org.immutables.value.Value.Immutable;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public class Ad {
+import com.github.saschawiegleb.ek.api.ImmutableAd.Builder;
 
-    public static Ad byElement(String id, Element backupElement) {
-        Ad element = new Ad();
-        element.setId(id);
-        return element;
-    }
+import javaslang.collection.List;
+import javaslang.collection.Seq;
+import javaslang.control.Either;
 
-    public static Ad byId(Long id, Element element) {
-        return byId(id.toString(), element);
-    }
+@Immutable
+abstract class Ad {
 
-    public static Ad byId(String id, Element backupElement) {
-        id = id.split("/")[id.split("/").length - 1]; // works for "id" and
-                                                      // "user-xxx-id"
-        Ad element = new Ad();
-        element.setId(id);
+    static Ad byId(long id, Element element) {
+        ImmutableAd.Builder builder = ImmutableAd.builder();
+        builder.id(id);
+        // TODO side effect, must be removed
         Document doc = Reader.requestDocument(linkById(id)).get();
 
         if (doc.getElementById("viewad-adexpired") != null || doc.getElementById("home") != null) {
-            System.out.println("already expired");
-            if (backupElement != null) {
-                return byElement(id, backupElement);
-            } else {
-                element.setHeadline("Artikel bereits verkauft.");
-                return element;
-            }
+            builder.headline("no longer available");
+            return builder.build();
         }
 
-        setCategory(element, doc);
-        setHeadline(element, doc);
-        setPrice(element, doc);
-        setImages(element, doc);
-        setDescription(element, doc);
-        setVendor(element, doc);
+        setCategory(builder, doc);
+        setHeadline(builder, doc);
+        setPrice(builder, doc);
+        setImages(builder, doc);
+        setDescription(builder, doc);
+        setVendor(builder, doc);
 
         Element details = getDetails(doc);
         // String price = details.getElementsByAttributeValue("itemprop",
@@ -50,163 +43,116 @@ public class Ad {
         // String currency = details.getElementsByAttributeValue("itemprop",
         // "currency").first().attr("content");
         // details.getElementsByClass("attributelist-striped").first().getElementsByTag("dt")
-        setLocation(element, details);
-        setDate(backupElement, element, details);
+        setLocation(builder, details);
+        setDate(element, builder, details);
         // String id =
         // details.getElementsByClass("attributelist-striped").first().getElementsByTag("dd").get(2).ownText();
         // details.getElementsByAttributeValue("itemprop", "price");
 
-        return element;
+        return builder.build();
     }
 
     private static Element getDetails(Document document) {
         return document.getElementById("viewad-details").getElementsByClass("l-container").first();
     }
 
-    public static URL linkById(String id) {
+    static URL linkById(long id) {
+        // TODO side effect, must be remove
         return Configuration.defaults().resolvePath("s-anzeige/" + id).get();
     }
 
-    private static void setCategory(Ad ad, Document document) {
-        String category[] = document.getElementById("vap-brdcrmb").getElementsByClass("breadcrump-link").last().attr("href")
-            .split("/");
-        ad.setCategory(category[category.length - 1].substring(1));
+    private static void setCategory(Builder builder, Document document) {
+        String category[] = document.getElementById("vap-brdcrmb").getElementsByClass("breadcrump-link").last().attr("href").split("/");
+        builder.category(category[category.length - 1].substring(1));
     }
 
-    private static void setDate(Element adFromList, Ad ad, Element details) {
-        String date = details.getElementsByClass("attributelist-striped").first().getElementsByTag("dd").get(1).ownText();
-        String time[] = adFromList.getElementsByClass("aditem-addon").first().ownText().split(",");
-        String timee = time[time.length - 1].trim();
-        ad.setTime(date + " " + timee);
-    }
-
-    private static void setDescription(Ad ad, Document document) {
-        ad.setDescription(document.getElementById("viewad-description-text").ownText());
-    }
-
-    private static void setHeadline(Ad ad, Document document) {
-        Element title = document.getElementById("viewad-title");
-        ad.setHeadline(title.ownText());
-    }
-
-    private static void setImages(Ad ad, Document document) {
-        if (document.getElementById("viewad-thumbnails") != null) {
-            Elements elements = document.getElementById("viewad-thumbnails").getElementsByTag("img");
-            List<String> images = new ArrayList<>();
-            for (Element img : elements) {
-                String link = img.attr("data-imgsrc").replaceFirst("_72", "_57");
-                images.add(link);
-            }
-            ad.setImages(images);
+    private static void setDate(Element adFromList, Builder builder, Element details) {
+        try {
+            String date = details.getElementsByClass("attributelist-striped").first().getElementsByTag("dd").get(1).ownText();
+            String time[] = adFromList.getElementsByClass("aditem-addon").first().ownText().split(",");
+            String timee = time[time.length - 1].trim();
+            LocalDateTime dateTime = LocalDateTime.parse(date + " " + timee, new DateTimeFormatterBuilder().appendPattern("dd.MM.yyyy HH:mm").toFormatter());
+            builder.time(Either.right(dateTime));
+        } catch (Exception e) {
+            builder.time(Either.left(e));
         }
     }
 
-    private static void setLocation(Ad ad, Element details) {
-        String ort = details.getElementsByClass("attributelist-striped").first().getElementsByTag("dd").get(0).getElementById("viewad-locality").ownText();
-        ad.setLocation(ort);
+    private static void setDescription(Builder builder, Document document) {
+        builder.description(document.getElementById("viewad-description-text").ownText());
     }
 
-    private static void setPrice(Ad ad, Document document) {
+    private static void setHeadline(Builder builder, Document document) {
+        Element title = document.getElementById("viewad-title");
+        builder.headline(title.ownText());
+    }
+
+    private static void setImages(Builder builder, Document document) {
+        if (document.getElementById("viewad-thumbnails") != null) {
+            Elements elements = document.getElementById("viewad-thumbnails").getElementsByTag("img");
+            List<String> images = List.empty();
+            for (Element img : elements) {
+                String link = img.attr("data-imgsrc").replaceFirst("_72", "_57");
+                images = images.append(link);
+            }
+            builder.images(images);
+        }
+    }
+
+    private static void setLocation(Builder builder, Element details) {
+        String ort = details
+            .getElementsByClass("attributelist-striped").first()
+            .getElementsByTag("dd").first()
+            .getElementById("viewad-locality")
+            .ownText();
+        builder.location(ort);
+    }
+
+    private static void setPrice(Builder builder, Document document) {
         Element price = document.getElementById("viewad-price");
-        ad.setPrice(price.ownText());
+        builder.price(price.ownText());
     }
 
-    private static void setVendor(Ad ad, Document document) {
-        ad.setVendorId(document.getElementById("viewad-contact").getElementsByClass("iconlist-text").first()
-            .getElementsByTag("a").first().attr("href").replaceAll("/s-bestandsliste\\.html\\?userId=", ""));
-        ad.setVendorName(document.getElementById("viewad-contact").getElementsByClass("iconlist-text").first()
-            .getElementsByTag("a").first().ownText());
+    private static void setVendor(Builder builder, Document document) {
+        Element link = document
+            .getElementById("viewad-contact")
+            .getElementsByClass("iconlist-text").first()
+            .getElementsByTag("a").first();
+        builder.vendorId(link.attr("href").replaceAll("/s-bestandsliste\\.html\\?userId=", ""));
+        builder.vendorName(link.ownText());
     }
 
-    private String category;
+    abstract String category();
 
-    private String description = "";
-
-    private String headline = "";
-
-    private String id;
-
-    private List<String> images;
-
-    private String location;
-
-    private String price;
-
-    private boolean printed = false;
-
-    private String subcategory;
-
-    private String time;
-
-    private String vendorId;
-
-    private String vendorName;
-
-    private Ad() {
+    @Default
+    String description() {
+        return "";
     }
 
-    public URL getLink() {
-        return linkById(id);
+    @Default
+    String headline() {
+        return "";
     }
 
-    public boolean isPrinted() {
-        return printed;
+    abstract long id();
+
+    abstract Seq<String> images();
+
+    final URL link() {
+        return linkById(id());
     }
 
-    public String searchString() {
-        return headline.toLowerCase() + " " + description.toLowerCase();
+    abstract String location();
+
+    abstract String price();
+
+    final String searchString() {
+        return headline().toLowerCase() + " " + description().toLowerCase();
     }
 
-    private void setCategory(String category) {
-        this.category = category;
-    }
+    abstract Either<Throwable, LocalDateTime> time();
 
-    private void setDescription(String description) {
-        this.description = description;
-    }
+    abstract String vendorId();
 
-    private void setHeadline(String title) {
-        this.headline = title;
-    }
-
-    private void setId(String id) {
-        this.id = id;
-    }
-
-    private void setImages(List<String> images) {
-        this.images = images;
-    }
-
-    private void setLocation(String location) {
-        this.location = location;
-    }
-
-    private void setPrice(String price) {
-        this.price = price;
-    }
-
-    public void setPrinted() {
-        this.printed = true;
-    }
-
-    private void setSubcategory(String subcategory) {
-        this.subcategory = subcategory;
-    }
-
-    private void setTime(String time) {
-        this.time = time;
-    }
-
-    private void setVendorId(String vendorId) {
-        this.vendorId = vendorId;
-    }
-
-    private void setVendorName(String vendorName) {
-        this.vendorName = vendorName;
-    }
-
-    @Override
-    public String toString() {
-        return "Ad [headline=" + headline + ", id=" + id + ", getLink()=" + getLink() + "]";
-    }
+    abstract String vendorName();
 }
