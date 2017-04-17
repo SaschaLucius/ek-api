b9dccea7760aa24f1bc98544be1dff9fde262784
Sascha Wiegleb <sascha.wiegleb@datameer.com>
2017-04-17 22:03:42 +0200

    Added functionality and stablility

--- src/main/java/com/github/saschawiegleb/ek/api/Parser.java
+++ src/main/java/com/github/saschawiegleb/ek/api/Parser.java
@@ -19,6 +19,10 @@ import javaslang.collection.Seq;
 import javaslang.control.Either;
 
 final class Parser {
+
+    private static final Element defaultElement = new Element("DEFAULT");
+    private static Elements defaultElements = new Elements();
+
     private final Configuration configuration;
 
     private Parser(Configuration configuration) {
@@ -39,9 +43,27 @@ final class Parser {
         return map;
     }
 
+    private static Elements selectAll(Element element, String selector) {
+        if (element != null && selector != null) {
+            Elements elements = element.select(selector);
+            if (elements.size() > 0) {
+                return elements;
+            }
+        }
+        return defaultElements;
+    }
+
+    private static Element selectFirst(Element element, String selector) {
+        Elements elements = selectAll(element, selector);
+        if (elements.size() == 1) {
+            return elements.first();
+        }
+        return defaultElement;
+    }
+
     private static void setAdditionslDetails(Builder builder, Document document) {
-        Elements keys = document.select("#viewad-details > section > dl dt");
-        Elements values = document.select("#viewad-details > section > dl dd");
+        Elements keys = selectAll(document, "#viewad-details > section > dl dt");
+        Elements values = selectAll(document, "#viewad-details > section > dl dd");
 
         Map<String, String> details = HashMap.empty();
         for (int i = 3; i < keys.size(); i++) {
@@ -55,7 +77,7 @@ final class Parser {
             }
             if (value.replaceAll(",", "").trim().isEmpty()) {
                 value = "";
-                for (Element e : values.get(i).select("a")) {
+                for (Element e : selectAll(values.get(i), "a")) {
                     value += e.ownText() + ",";
                 }
             }
@@ -65,16 +87,16 @@ final class Parser {
     }
 
     private static void setDescription(Builder builder, Document document) {
-        builder.description(document.select("#viewad-description-text").first().ownText());
+        builder.description(selectFirst(document, "#viewad-description-text").ownText());
     }
 
     private static void setHeadline(Builder builder, Document document) {
-        builder.headline(document.select("#viewad-title").first().ownText());
+        builder.headline(selectFirst(document, "#viewad-title").ownText());
     }
 
     private static void setImages(Builder builder, Document document) {
-        if (document.select("#viewad-thumbnails") != null) {
-            Elements elements = document.select("#viewad-thumbnail-list img");
+        if (!selectFirst(document, "#viewad-thumbnails").equals(defaultElement)) {
+            Elements elements = selectAll(document, "#viewad-thumbnail-list img");
             List<String> images = List.empty();
             for (Element img : elements) {
                 String link = img.attr("data-imgsrc").replaceFirst("_72", "_57");
@@ -85,17 +107,30 @@ final class Parser {
     }
 
     private static void setLocation(Builder builder, Document document) {
-        builder.location(document.select("#viewad-locality").first().ownText());
+        builder.location(selectFirst(document, "#viewad-locality").ownText());
     }
 
     private static void setPrice(Builder builder, Document document) {
-        builder.price(document.select("#viewad-price").first().ownText().replaceAll("Preis: ", ""));
-    }
 
-    private static void setVendor(Builder builder, Document document) {
-        Element link = document.select("#viewad-contact > div > ul > li:nth-child(1) > span > span.text-bold.text-bigger.text-force-linebreak > a").first();
-        builder.vendorId(link.attr("href").replaceAll("/s-bestandsliste\\.html\\?userId=", ""));
-        builder.vendorName(link.ownText());
+        Element element = selectFirst(document, "#viewad-price");
+        if (!element.equals(defaultElement)) {
+            builder.price(element.ownText().replaceAll("Preis: ", ""));
+            return;
+        }
+
+        String category = selectFirst(document, "#viewad-main > meta").attr("content");
+        if (category.equals("Tauschen") || category.equals("Zu verschenken") || category.equals("Verleihen")) {
+            builder.price(category);
+            return;
+        }
+
+        String jobs = selectFirst(document, "#vap-brdcrmb > a:nth-child(2) > span").ownText();
+        if (jobs.equals("Jobs")) {
+            builder.price("job");
+            return;
+        }
+
+        builder.price("unknown");
     }
 
     private static Either<String, LocalDateTime> time(Element element) {
@@ -130,13 +165,19 @@ final class Parser {
         return configuration.resolvePath("s-anzeige/" + id).get();
     }
 
+    private URL linkByUserId(String id) {
+        if (id.contains("shop")) {
+            return configuration.resolvePath(id).get();
+        }
+        return configuration.resolvePath("s-bestandsliste.html?userId=" + id).get();
+    }
+
     Ad readAd(long id, Either<String, LocalDateTime> time) {
         ImmutableAd.Builder builder = ImmutableAd.builder();
         builder.id(id);
         builder.time(time);
         // TODO side effect, must be removed
         Document doc = Reader.requestDocument(linkById(id)).get();
-
         if (!isAvailable(doc)) {
             builder.headline("no longer available");
             return builder.build();
@@ -153,10 +194,45 @@ final class Parser {
         return builder.build();
     }
 
+    Ad readAdSmall(long id, Either<String, LocalDateTime> time, Element element) {
+        ImmutableAd.Builder builder = ImmutableAd.builder();
+        builder.id(id);
+        builder.time(time);
+        builder.headline(selectFirst(element, "article > section.aditem-main > h2 > a").ownText());
+        builder.description(selectFirst(element, "article > section.aditem-main > p:nth-child(2)").ownText());
+        builder.price(selectFirst(element, "article > section.aditem-details > strong").ownText());
+        builder.location(selectFirst(element, "article > section.aditem-details").ownText());
+        builder.time(time(element));
+        // String img = selectFirst(element, "article > section.aditem-image
+        // > div > img").attr("data-imgsrc");
+        return builder.build();
+    }
+
     private void setCategory(Builder builder, Document document) {
-        String category[] = document.select("#vap-brdcrmb > a:nth-last-child(1)").first().attr("href").split("/");
-        int id = Integer.parseInt(category[category.length - 1].substring(1));
-        builder.category(configuration.category(id).get());
+        String category[] = selectFirst(document, "#vap-brdcrmb > a:nth-last-child(1)").attr("href").split("/");
+        if (category.length > 0 && !category[category.length - 1].isEmpty()) {
+            int id = Integer.parseInt(category[category.length - 1].substring(1));
+            builder.category(configuration.category(id).get());
+        }
+    }
+
+    private void setVendor(Builder builder, Document document) {
+        Element link = selectFirst(document, "#viewad-contact > div > ul > li:nth-child(1) > span > span.text-bold.text-bigger.text-force-linebreak > a");
+        if (!link.equals(defaultElement)) {
+            builder.vendorId(link.attr("href").replaceAll("/s-bestandsliste\\.html\\?userId=", ""));
+            builder.vendorName(link.ownText());
+            return;
+        }
+        link = selectFirst(document, "#poster-other-ads-link");
+        if (!link.equals(defaultElement)) {
+            String id = link.attr("href").replaceAll("/s-bestandsliste\\.html\\?userId=", "");
+            builder.vendorId(id);
+            Document userPage = Reader.requestDocument(linkByUserId(id)).get();
+            Element user = selectFirst(userPage, "#site-content > div.l-splitpage > div.l-splitpage-navigation > section > header > h2");
+            builder.vendorName(user.ownText());
+            return;
+        }
+        System.out.println(document.baseUri());
     }
 
 }
--- src/test/java/com/github/saschawiegleb/ek/api/AdTest.java
+++ src/test/java/com/github/saschawiegleb/ek/api/AdTest.java
@@ -3,6 +3,9 @@ package com.github.saschawiegleb.ek.api;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 import org.jsoup.nodes.Document;
 import org.junit.Test;
@@ -39,6 +42,28 @@ public class AdTest implements DefaultConfiguration {
     public void testAdByIdExpired() {
         Ad ad = Parser.of(defaultConfiguration).readAd(428021741L, Either.left("no time set"));
         assertThat(ad).isEqualTo(ImmutableAd.builder().id(428021741L).headline("no longer available").build());
+    }
+
+    @Test
+    public void testRandomAdById() {
+        List<Ad> ads = new ArrayList<>();
+        long x = 600000000L;
+        long y = 634606820L;
+        Random r = new Random();
+
+        for (int i = 0; i < 100; i++) {
+            long number = x + (long) (r.nextDouble() * (y - x));
+            Ad ad = Parser.of(defaultConfiguration).readAd(number, Either.left("no time set"));
+            ads.add(ad);
+        }
+
+        List<Ad> availebleAds = new ArrayList<>();
+        for (Ad ad : ads) {
+            if (!ad.headline().isEmpty()) {
+                availebleAds.add(ad);
+            }
+        }
+        assertThat(availebleAds).isNotEmpty();
     }
 
 }
